b372f8567c750741d918e6ebd5ba9fde5dea0c6e
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-07-06 04:17:47 +0900

    Refactor: 404 페이지 템플릿 적용

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,12 +1,10 @@
+import { notFoundTemplate } from '../templates/notFoundPageTemplates'
+
 export default function NotFoundPage({ $target }) {
   const $notFoundPage = document.createElement('div')
   $target.appendChild($notFoundPage)
 
-  $notFoundPage.innerHTML = `
-    <img src="#" />
-    <p>이 콘텐츠는 존재하지 않습니다</p>
-    <button>내 콘텐츠로 돌아가기</button>
-  `
+  $notFoundPage.innerHTML = notFoundTemplate()
 
   $notFoundPage.addEventListener('click', () => {
     // navigate('/')
---
+++ src/templates/notFoundPageTemplates.js
@@ -0,0 +1,7 @@
+export function notFoundTemplate() {
+  return `
+    <img src="#" />
+    <p>이 콘텐츠는 존재하지 않습니다</p>
+    <button>내 콘텐츠로 돌아가기</button>
+  `
+}
