45239518e036890989b938522e01b9b5eecb4d31
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-07-15 06:24:53 +0900

    Refactor: validation 함수 수정, 테스트 코드 수정

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,11 +1,7 @@
 import { ErrorMessages } from './constants'
 
 export function validateNewInstance(name, target) {
-  try {
-    if (!target) {
-      throw new Error(`${name} ${ErrorMessages.INVALID_NEW_OPERATION}`)
-    }
-  } catch (e) {
-    console.log(e.message)
+  if (!target) {
+    throw new Error(`${name} ${ErrorMessages.INVALID_NEW_OPERATION}`)
   }
 }
--- src/utils/validation.spec.js
+++ src/utils/validation.spec.js
@@ -1,14 +1,23 @@
 import { ErrorMessages } from './constants'
 import { validateNewInstance } from './validation'
 
+jest.mock('./constants', () => ({
+  ErrorMessages: {
+    INVALID_NEW_OPERATION: '컴포넌트 생성 시 new 연산자를 붙이지 않았습니다.',
+  },
+}))
+
 describe('validateNewInstance', () => {
   test('new 연산자를 사용한 경우 올바른 인스턴스를 반환해야 합니다', () => {
     function TestComponent() {
       validateNewInstance('TestComponent', new.target)
     }
 
-    const instance = new TestComponent()
+    expect(() => {
+      new TestComponent()
+    }).not.toThrow()
 
+    const instance = new TestComponent()
     expect(instance.constructor).toBe(TestComponent)
   })
 
