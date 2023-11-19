79638115d6ea82b735a39b83515ec71749e9a674
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-20 05:14:54 +0900

    Design: 404 페이지 위치 수정

--- src/templates/notFoundPageTemplates.js
+++ src/templates/notFoundPageTemplates.js
@@ -1,9 +1,7 @@
 export function notFoundTemplate() {
   return `
-    <div class='not-found'>
-      <img src='/assets/images/eyes.svg' />
-      <p>이 콘텐츠는 존재하지 않습니다</p>
-      <button>내 콘텐츠로 돌아가기</button>
-    </div>
+    <img src='/assets/images/eyes.svg' />
+    <p>이 콘텐츠는 존재하지 않습니다</p>
+    <button>내 콘텐츠로 돌아가기</button>
   `
 }
